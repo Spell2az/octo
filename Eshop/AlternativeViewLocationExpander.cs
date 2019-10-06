7e75dc8dc49f1cd0725b84480e511bd5e538e2d8
martinK <martin_kovac@outlook.com>
2019-10-06 21:27:33 +0100

    Add ViewLocationExpadner

---
+++ Eshop/AlternativeViewLocationExpander.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Razor;
+
+namespace Eshop
+{
+    public class AlternativeViewLocationExpander : IViewLocationExpander
+    {
+        private const string ValueKey = "alt";
+
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            context.Values.TryGetValue(ValueKey, out var value);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                return viewLocations;
+            }
+
+            return ExpandViewLocationsCore(viewLocations, value);
+        }
+
+        private IEnumerable<string> ExpandViewLocationsCore(IEnumerable<string> viewLocations, string value)
+        {
+            foreach (var location in viewLocations)
+            {
+                yield return location.Replace("{0}", value + "/{0}");
+                yield return location;
+            }
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            context.Values[ValueKey] = context.ActionContext.RouteData.Values[ValueKey]?.ToString();
+        }
+    }
+}
---
+++ Eshop/CustomViewLocationExpander.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc.Razor;
+using Microsoft.VisualStudio.Web.CodeGenerators.Mvc.Controller;
+
+namespace Eshop
+{
+    public class CustomViewLocationExpander : IViewLocationExpander
+    {
+        public IEnumerable<string> ExpandViewLocations(ViewLocationExpanderContext context, IEnumerable<string> viewLocations)
+        {
+            var updatedViewLocations = viewLocations.ToList();
+            updatedViewLocations.Add("/Views/{0}.cshtml");
+            updatedViewLocations.Add("/Views/Shared/{0}.cshtml");
+
+            return updatedViewLocations;
+        }
+
+        public void PopulateValues(ViewLocationExpanderContext context)
+        {
+            Console.WriteLine(context);
+        }
+    }
+}
